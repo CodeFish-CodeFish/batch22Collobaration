c0395687ae570743342cc4f80f6d195e96460494
meryemvarli <meryemvarli@gmail.com>
2024-07-22 19:58:57 -0400

    added my class1

--- src/main/java/meryem.java
+++ src/main/java/meryem.java
@@ -1,7 +1,7 @@
 public class meryem {
     public static void main(String[] args) {
         System.out.println("git hello");
-        System.out.println("hii");
+        System.out.println("Hii");
 
     }
 }
